d89f5a4dc8b3ea23be471c7e301ec54f4ec2f213
Dylan <lightfury954@gmail.com>
2025-11-18 02:06:53 -0500

    Initializes REST API with database connection
    Sets up a basic REST API framework.
    
    Establishes a connection to a MySQL database for data storage.
    
    Introduces a sample data insertion for testing the database connectivity.
    
    Adds .env file to gitignore to prevent the password being pushed to github
    
    Still needs implementation for pulling data from the sql table.

--- REST_api.py
+++ REST_api.py
@@ -1,2 +1,32 @@
 import mysql.connector
 import flask
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+password = os.getenv("PASSWORD")
+
+
+db = mysql.connector.connect(user='root', password=password, host="localhost", database="bloggingplatformdb")
+
+test_dict = {
+    "name" : "this is a test name",
+    "title": "this is a test title" #TODO: Consider switching from sending data fully as json to splitting into seperate MySQL Tables
+}
+
+test_json = json.dumps(test_dict)
+
+dbcursor = db.cursor()
+
+sql = "INSERT INTO posts (idblog, dataBlog) VALUES (%s, %s)"
+val = [(3, test_json)]
+dbcursor.executemany(sql, val)
+
+#TODO: Now create way to pull data from SQL table
+
+db.commit()
+
+dbcursor.close()
+db.close()
