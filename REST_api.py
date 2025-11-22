6be7dd4fe2a02750a07168c8d084217ed2cc9713
Dylan <lightfury954@gmail.com>
2025-11-21 23:43:10 -0500

    Adds database interaction functions
    Adds functions for inserting and retrieving blog posts
    from the database.
    
    Includes functionality for formatting datetime objects
    when retrieving data.

--- REST_api.py
+++ REST_api.py
@@ -3,6 +3,7 @@ import flask
 from dotenv import load_dotenv
 import os
 import json
+import datetime
 
 load_dotenv()
 
@@ -14,23 +15,44 @@ db = mysql.connector.connect(user='root', password=password, host="localhost", d
 
 
 test_dict = {
-    "id": 2,
     "title" : "this is a test name",
     "content": "this is a test title",
     "category": "this is the category",
     "tags": ["tag 1", "tag 2"]
 }
 
-test_json = json.dumps(test_dict)
 
-dbcursor = db.cursor()
 
-sql = "INSERT INTO posts (blogId, blogTitle, blogContent, blogCategory, blogTags) VALUES (%s, %s, %s, %s, %s)"
-dbcursor.execute(sql, (test_dict["id"],test_dict["title"], test_dict["content"], test_dict["category"], json.dumps(test_dict["tags"])))
+def db_insert(test_dict, db):
 
-#TODO: Now create way to pull data from SQL table
+    dbcursor = db.cursor()
 
-db.commit()
+    sql = "INSERT INTO posts (blogTitle, blogContent, blogCategory, blogTags) VALUES (%s, %s, %s, %s)"
+    dbcursor.execute(sql, (test_dict["title"], test_dict["content"], test_dict["category"], json.dumps(test_dict["tags"])))
 
-dbcursor.close()
-db.close()
+    db.commit()
+    dbcursor.close()
+    db.close()
+
+def db_retrieve(db_id, db):
+    dbcursor = db.cursor()
+
+    sql =  "SELECT * FROM posts WHERE blogId = %s"
+    dbcursor.execute(sql, (db_id,))
+    result = dbcursor.fetchone()
+    retrieved_dict = {
+        "id": result[0],
+        "title": result[1],
+        "content": result[2],
+        "category": result[3],
+        "tags": result[4],
+        "timeCreated": result[5].strftime("%Y-%m-%d %H:%M:%S.%f"),
+        "timeUpdated": result[6].strftime("%Y-%m-%d %H:%M:%S.%f")
+    }
+    print(retrieved_dict)
+
+    dbcursor.close()
+    db.close()
+
+db_retrieve(3, db)
+#db_insert(test_dict, db)
