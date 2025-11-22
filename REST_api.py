1ed8e864968e84109b1f866017204e3aaf9fde57
Dylan <lightfury954@gmail.com>
2025-11-21 22:54:13 -0500

    Updates database insertion logic
    Modifies the database insertion method to align with the new schema.
    Changes from using a single JSON blob to individual columns for blog details.
    Also updates the test data to match the new schema.

--- REST_api.py
+++ REST_api.py
@@ -11,18 +11,22 @@ password = os.getenv("PASSWORD")
 
 db = mysql.connector.connect(user='root', password=password, host="localhost", database="bloggingplatformdb")
 
+
+
 test_dict = {
-    "name" : "this is a test name",
-    "title": "this is a test title" #TODO: Consider switching from sending data fully as json to splitting into seperate MySQL Tables
+    "id": 2,
+    "title" : "this is a test name",
+    "content": "this is a test title",
+    "category": "this is the category",
+    "tags": ["tag 1", "tag 2"]
 }
 
 test_json = json.dumps(test_dict)
 
 dbcursor = db.cursor()
 
-sql = "INSERT INTO posts (idblog, dataBlog) VALUES (%s, %s)"
-val = [(3, test_json)]
-dbcursor.executemany(sql, val)
+sql = "INSERT INTO posts (blogId, blogTitle, blogContent, blogCategory, blogTags) VALUES (%s, %s, %s, %s, %s)"
+dbcursor.execute(sql, (test_dict["id"],test_dict["title"], test_dict["content"], test_dict["category"], json.dumps(test_dict["tags"])))
 
 #TODO: Now create way to pull data from SQL table
 
