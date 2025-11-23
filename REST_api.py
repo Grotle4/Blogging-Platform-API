c352fe95ea5118816c6af846b3f0c4b75c67788a
Dylan <lightfury954@gmail.com>
2025-11-23 01:32:20 -0500

    Enables editing of existing blog posts
    Adds functionality to update existing blog posts in the database.
    Implements PUT request handling for posts, allowing users to modify
    the title, content, category, and tags of a post. Returns a 200
    status code upon successful update.

--- REST_api.py
+++ REST_api.py
@@ -53,32 +53,50 @@ def db_retrieve(db_id):
     return retrieved_dict
 
 
+def db_edit(retrieved_item, db_id):
+    db = mysql.connector.connect(user='root', password=password, host="localhost", database="bloggingplatformdb")
+    dbcursor = db.cursor()
+
+    sql =  "UPDATE posts SET blogTitle = %s, blogContent = %s, blogCategory = %s, blogTags = %s WHERE blogId = %s"
+    dbcursor.execute(sql, (retrieved_item["title"], retrieved_item["content"], retrieved_item["category"], retrieved_item["tags"], db_id))
+
+    db.commit()
+
+    dbcursor.close()
+    db.close()
+
 @app.route("/posts", methods=['POST'])
 def create_blog():
     if request.method == 'POST':
         data = request.get_json()
         db_insert(data)
         print("POST Request")
-        return jsonify("POSTED")
+        return "Post Created", 201
 
 
 
 @app.route("/posts/<int:item_id>", methods=['GET', 'PUT', 'DELETE'])
 def call_blog(item_id):
     match request.method:
-        case 'GET':
+        case 'GET': #TODO: Add seperate route where terms can be looked up to find all articles based on tags
             print("GET Request")
             retrieved_item = db_retrieve(item_id)
-            return(jsonify(f"GETTED item #{item_id}: {retrieved_item}"))
-        case 'PUT': #TODO: Add ability to retrieve from db and then edit existing data, returning it to db with update time changed
+            return jsonify(retrieved_item), 200
+        case 'PUT':
             print("PUT Request")
-            print(item_id)
-            return(jsonify(f"PUTTED {item_id}"))
+            retrieved_item = db_retrieve(item_id)
+            data = request.get_json()
+            retrieved_item["title"] = data["title"]
+            retrieved_item["content"] = data["content"]
+            retrieved_item["category"] = data["category"]
+            retrieved_item["tags"] = json.dumps(data["tags"])
+            db_edit(retrieved_item, item_id)
+            return "Blog Post edited successfully", 200
         case 'DELETE': #Add ability to delete an entry from the db based on id
             print("DELETE Request")
             print(item_id)
             return(jsonify(f"DELETED {item_id}"))
-
+#TODO: Add error handing where error code 404 is returned on invalid id or no location found
 
 if __name__ == '__main__':
     app.run()
