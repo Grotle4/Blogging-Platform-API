46c7ae3402c1a1758fd7ecc4d042f18fe49a4c45
Dylan <lightfury954@gmail.com>
2025-11-22 21:29:16 -0500

    Creates REST API endpoints for blog posts
    Implements POST and GET requests for blog posts.
    
    The changes introduce Flask to create REST API endpoints.
    It also modifies the db interaction, handling the db connection inline within the functions.
    The /posts endpoint enables the creation of new blog entries using POST requests.
    The /posts/ endpoint now supports GET requests to retrieve specific blog posts using an ID.

--- REST_api.py
+++ REST_api.py
@@ -1,5 +1,5 @@
 import mysql.connector
-import flask
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 import os
 import json
@@ -9,32 +9,29 @@ load_dotenv()
 
 password = os.getenv("PASSWORD")
 
-
-db = mysql.connector.connect(user='root', password=password, host="localhost", database="bloggingplatformdb")
-
+app = Flask(__name__)
 
 
 test_dict = {
     "title" : "this is a test name",
-    "content": "this is a test title",
+    "content": "this is test content",
     "category": "this is the category",
     "tags": ["tag 1", "tag 2"]
 }
 
-
-
-def db_insert(test_dict, db):
-
+def db_insert(post_dict):
+    db = mysql.connector.connect(user='root', password=password, host="localhost", database="bloggingplatformdb")
     dbcursor = db.cursor()
 
     sql = "INSERT INTO posts (blogTitle, blogContent, blogCategory, blogTags) VALUES (%s, %s, %s, %s)"
-    dbcursor.execute(sql, (test_dict["title"], test_dict["content"], test_dict["category"], json.dumps(test_dict["tags"])))
+    dbcursor.execute(sql, (post_dict["title"], post_dict["content"], post_dict["category"], json.dumps(post_dict["tags"])))
 
     db.commit()
     dbcursor.close()
     db.close()
 
-def db_retrieve(db_id, db):
+def db_retrieve(db_id):
+    db = mysql.connector.connect(user='root', password=password, host="localhost", database="bloggingplatformdb")
     dbcursor = db.cursor()
 
     sql =  "SELECT * FROM posts WHERE blogId = %s"
@@ -53,6 +50,35 @@ def db_retrieve(db_id, db):
 
     dbcursor.close()
     db.close()
+    return retrieved_dict
 
-db_retrieve(3, db)
-#db_insert(test_dict, db)
+
+@app.route("/posts", methods=['POST'])
+def create_blog():
+    if request.method == 'POST':
+        data = request.get_json()
+        db_insert(data)
+        print("POST Request")
+        return jsonify("POSTED")
+
+
+
+@app.route("/posts/<int:item_id>", methods=['GET', 'PUT', 'DELETE'])
+def call_blog(item_id):
+    match request.method:
+        case 'GET':
+            print("GET Request")
+            retrieved_item = db_retrieve(item_id)
+            return(jsonify(f"GETTED item #{item_id}: {retrieved_item}"))
+        case 'PUT': #TODO: Add ability to retrieve from db and then edit existing data, returning it to db with update time changed
+            print("PUT Request")
+            print(item_id)
+            return(jsonify(f"PUTTED {item_id}"))
+        case 'DELETE': #Add ability to delete an entry from the db based on id
+            print("DELETE Request")
+            print(item_id)
+            return(jsonify(f"DELETED {item_id}"))
+
+
+if __name__ == '__main__':
+    app.run()
